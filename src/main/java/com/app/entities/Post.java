6478232bb4dbfc8200b25c13f6b97b1340905553
Aniket Dalal <58764738+aniket691@users.noreply.github.com>
2024-05-23 23:35:27 +0530

    implemented posts crud

--- src/main/java/com/app/entities/Post.java
+++ src/main/java/com/app/entities/Post.java
@@ -22,5 +22,62 @@ public class Post {
 	@JoinColumn(name = "category_id")
 	private Category category;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
 	// Getters and Setters
+
 }
--- src/main/java/com/app/tester/TestHibernate.java
+++ src/main/java/com/app/tester/TestHibernate.java
@@ -1,17 +1,39 @@
 package com.app.tester;
 
-import static com.app.utils.HibernateUtils.getFactory;
+import java.util.List;
 
-import org.hibernate.SessionFactory;
+import com.app.dao.PostDao;
+import com.app.dao.PostDaoImpl;
+import com.app.entities.Post;
 
 public class TestHibernate {
 
 	public static void main(String[] args) {
-		try (SessionFactory sf = getFactory()) {
-			System.out.println("Hibernate frmwork loaded " + sf);
-		} //JVM sf.close => cleanig up of DBCP
-		catch (Exception e) {
-			e.printStackTrace();
+		PostDao postDao = new PostDaoImpl();
+
+		// Create a new post
+		Post post = new Post();
+		post.setContent("This is the content of the post.");
+		post.setDescription("This is the description of the post.");
+		post.setImagePath("/images/sample.png");
+		post.setTitle("Sample Post Title");
+		postDao.createPost(post);
+
+		// Read a post
+		Post retrievedPost = postDao.getPost(post.getId());
+		System.out.println("Retrieved Post Title: " + retrievedPost.getTitle());
+
+		// Update a post
+		retrievedPost.setTitle("Updated Post Title");
+		postDao.updatePost(retrievedPost);
+
+		// Delete a post
+//		postDao.deletePost(retrievedPost.getId());
+
+		// List all posts
+		List<Post> allPosts = postDao.getAllPosts();
+		for (Post p : allPosts) {
+			System.out.println("Post Title: " + p.getTitle());
 		}
 
 	}
